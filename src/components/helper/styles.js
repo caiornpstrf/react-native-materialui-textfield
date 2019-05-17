29db9ecffd66dbf4fdca42243395e8a0916dd114
Caio Reis <31257987+caiornpstrf@users.noreply.github.com>
2019-05-17 16:13:44 -0300

    Update styles.js

--- src/components/helper/styles.js
+++ src/components/helper/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
+    //...StyleSheet.absoluteFillObject,
     paddingVertical: 4,
     alignItems: 'flex-start',
   },
